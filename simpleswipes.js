06d6ab20a86e9395a96b78d592aba9e88e4abee2
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-01 18:18:01 +0300

    create event even if stop event is missed

--- simpleswipes.js
+++ simpleswipes.js
@@ -23,8 +23,10 @@ function SimpleSwipes(controller) {
     this.emit = _.throttle(this.emit, 500);
 
     this.start = null;
+    this.lastUpdate = null;
     this.stopWaiter = null;
     this.controller.on("frame", function(frame) {
+        if (!frame.data.gestures) return;
         if (frame.data.gestures.length === 0) return;
 
         var gesture = frame.data.gestures[0];
@@ -47,43 +49,54 @@ util.inherits(SimpleSwipes, EventEmitter);
 
 SimpleSwipes.prototype.handleSwipe = function(gesture) {
 
+    if (gesture.state === "update") {
+        this.lastUpdate = gesture;
+    }
+
     if (gesture.state === "start" && !this.start) {
         this.start = gesture;
         this.stopWaiter = setTimeout(function() {
-            console.warn("never got stop");
-            this.end();
+            console.warn("ending with update");
+            this.handleSwipeStop(this.lastUpdate);
+
         }.bind(this), 1000);
 
         return;
     }
 
     if (!this.start) return;
+
     if (gesture.state === "stop" && this.start.id === gesture.id) {
-        var xdiff = this.start.position[0] - gesture.position[0];
-        var ydiff = this.start.position[1] - gesture.position[1];
-
-        if (isSmall(xdiff) && isSmall(ydiff)) {
-            console.warn("too small movement");
-            return this.end();
-        }
-
-        if (Math.abs(xdiff) > Math.abs(ydiff)) {
-            if (xdiff < 0) this.emit("left");
-            else this.emit("right");
-        }
-        else {
-            if (ydiff > 0) this.emit("up");
-            else this.emit("down");
-        }
-
-        this.end();
-        return;
+        this.handleSwipeStop(gesture);
     }
 
 };
 
+SimpleSwipes.prototype.handleSwipeStop = function(gesture) {
+
+    var xdiff = this.start.position[0] - gesture.position[0];
+    var ydiff = this.start.position[1] - gesture.position[1];
+
+    if (isSmall(xdiff) && isSmall(ydiff)) {
+        console.warn("too small movement");
+        return this.end();
+    }
+
+    if (Math.abs(xdiff) > Math.abs(ydiff)) {
+        if (xdiff < 0) this.emit("left");
+        else this.emit("right");
+    }
+    else {
+        if (ydiff > 0) this.emit("up");
+        else this.emit("down");
+    }
+
+    this.end();
+};
+
 SimpleSwipes.prototype.end = function() {
     this.start = null;
+    this.lastUpdate = null;
     clearTimeout(this.stopWaiter);
 };
 
