e09baf09ce9737386d6c746d68c72f9388ec2c79
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-02 13:53:13 +0300

    fix down key

--- bindkeys.js
+++ bindkeys.js
@@ -1,10 +1,8 @@
 var exec = require("child_process").exec;
 
 /**
- *
  * Bind SimpleSwipes events to keyboard strokes
  * @param {SimpleSwipes} ss
- *
  **/
 module.exports = function(ss) {
     ss.on("left", function() {
@@ -17,8 +15,8 @@ module.exports = function(ss) {
         console.log("up");
         exec("xdotool key Up");
     }).on("down", function() {
-        console.log("up");
-        exec("xdotool key Up");
+        console.log("down");
+        exec("xdotool key Down");
     }).on("point", function() {
         console.log("point");
         exec("xdotool key KP_Enter");
