75d62452f1e203a4e295e0e0be64b6b7e87326dc
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-05 15:33:41 +0300

    Add options bindings

---
+++ bindoptions.js
@@ -0,0 +1,26 @@
+
+/**
+ * @param {SimpleSwipes} ss
+ **/
+module.exports = function(ss) {
+    var d = window.document;
+    var store = window.localStorage;
+    var xEl = d.querySelector(".swapx");
+    var yEl = d.querySelector(".swapy");
+
+    xEl.checked = store.swapx;
+    yEl.checked = store.swapy;
+    ss.swapX(xEl.checked);
+    ss.swapY(yEl.checked);
+
+    xEl.addEventListener("change", function(e) {
+        ss.swapX(xEl.checked);
+        store.swapx = xEl.checked;
+    }, false);
+
+    yEl.addEventListener("change", function(e) {
+        ss.swapY(yEl.checked);
+        store.swapy = yEl.checked;
+    }, false);
+
+};
