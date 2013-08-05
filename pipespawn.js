1c92abd32c50d6867cdfe30feb4f465a17e771e4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-05 15:33:51 +0300

    add promise spawn wrapper

---
+++ pipespawn.js
@@ -0,0 +1,23 @@
+
+var spawn = require("child_process").spawn;
+var PassThrough = require('stream').PassThrough;
+var Q = require("q");
+
+
+module.exports = function(cmd, args) {
+  var d = Q.defer();
+  var p = spawn(cmd, args || [], {
+      detached: false // leapd and friends should die when Eleet dies
+  });
+
+  p.on("error", d.reject);
+  p.on("exit", d.resolve);
+
+  d.promise.stream = new PassThrough();
+  p.stdout.pipe(d.promise.stream);
+  p.stderr.pipe(d.promise.stream);
+
+  return d.promise;
+};
+
+
