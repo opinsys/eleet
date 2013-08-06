f0a851d0d1bcabc1b7eea6808a1593ead0b6fa31
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-06 13:04:50 +0300

    fix localstorage option save

--- bindoptions.js
+++ bindoptions.js
@@ -8,19 +8,19 @@ module.exports = function(ss) {
     var xEl = d.querySelector(".swapx");
     var yEl = d.querySelector(".swapy");
 
-    xEl.checked = store.swapx;
-    yEl.checked = store.swapy;
-    ss.swapX(xEl.checked);
-    ss.swapY(yEl.checked);
+    xEl.checked = !!store.swapx;
+    yEl.checked = !!store.swapy;
+    ss.swapX(!!xEl.checked);
+    ss.swapY(!!yEl.checked);
 
     xEl.addEventListener("change", function(e) {
         ss.swapX(xEl.checked);
-        store.swapx = xEl.checked;
+        store.swapx = xEl.checked ? "1" : "";
     }, false);
 
     yEl.addEventListener("change", function(e) {
         ss.swapY(yEl.checked);
-        store.swapy = yEl.checked;
+        store.swapy = yEl.checked ? "1" : "";
     }, false);
 
 };
