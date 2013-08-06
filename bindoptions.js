2920322da5096b58aaa2b2c8921a2f268eb106fc
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-08-06 13:42:18 +0300

    add option to disable

--- bindoptions.js
+++ bindoptions.js
@@ -7,11 +7,19 @@ module.exports = function(ss) {
     var store = window.localStorage;
     var xEl = d.querySelector(".swapx");
     var yEl = d.querySelector(".swapy");
+    var activeEl = d.querySelector(".active");
+
+    if (typeof store.active === "undefined") {
+        store.active = "1";
+    }
 
     xEl.checked = !!store.swapx;
     yEl.checked = !!store.swapy;
+    activeEl.checked = !!store.active;
+
     ss.swapX(!!xEl.checked);
     ss.swapY(!!yEl.checked);
+    ss.active = !!activeEl.checked;
 
     xEl.addEventListener("change", function(e) {
         ss.swapX(xEl.checked);
@@ -21,6 +29,10 @@ module.exports = function(ss) {
     yEl.addEventListener("change", function(e) {
         ss.swapY(yEl.checked);
         store.swapy = yEl.checked ? "1" : "";
+    }, false);
+
+    activeEl.addEventListener("change", function(e) {
+        ss.active = store.active = activeEl.checked ? "1" : "";
     }, false);
 
 };
